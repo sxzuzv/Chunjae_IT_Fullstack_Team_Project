858b99f852f24f63efb7e974934fc8e8a3c85511
Haeun Noh <00blowup@gmail.com>
2023-11-04 20:52:14 +0900

    chore: HuruTMain.java에 각자의 사용 구획을 나눔

--- src/main/HuruTMain.java
+++ src/main/HuruTMain.java
@@ -6,36 +6,65 @@ import java.util.Scanner;
 public class HuruTMain {
     
     // 필요한 서비스를 멤버로 받아오기
-    private static StudentService studentService = new StudentService();
 
+    // jy
+
+    
+    // jh
+    
+    
+    // sz
+
+    
+    // ys
+
+    
+    // he
+    
+
+
+    // 필요한 기능을 메소드로 만들기
+
+    // jy
+
+    
+    // jh
+    
+    
+    // sz
+
+    
+    // ys
+
+    
+    // he
+    
+
+
+    
+    
     public static void main(String[] args) {
-        // 메인 메소드. 프로그램을 시작하기 위해 이 메인 메소드를 실행시켜야 한다.
+        // 메인 메소드. 프로그램을 시작하기 위해 이 메인 메소드를 실행시켜야 한다.\
+
+        // 사용할 실행 코드를 작성하기
+
+        // jy
 
 
-        Scanner sc = new Scanner(System.in);
-
-        // 메인 화면을 콘솔에 출력하는 코드 작성
-        System.out.println("*****************************************");
-        System.out.print("환영 메시지");
-        System.out.println("*****************************************");
-
-        System.out.println("1. 로그인");
-        System.out.println("2. 회원가입");
-
-        // 사용자가 원하는 메뉴를 고르도록 하는 코드 작성
-        System.out.print("원하는 메뉴의 번호를 정수로 입력하세요: ");
-        int input = sc.nextInt();
+        // jh
 
 
-        // if-else if-else문 또는 switch-case문으로, 각 메뉴마다 해당하는 Service 인스턴스 내의 메소드를 호출
+        // sz
 
-        // 비어있는 조건문을 만든 상태에서 코드를 나눠받은 다음,
-        // 본인이 맡은 분기에 해당하는 공간을 호출문으로 채워주시면 됩니다!
-        if(input==1) {
-            // 로그인 맡은 사람이 채우기
-        } else if (input==2) {
-            // 회원가입 맡은 사람이 채우기
-        }
+
+        // ys
+
+
+        // he
+
+
+        
+        
     }
     
 }
