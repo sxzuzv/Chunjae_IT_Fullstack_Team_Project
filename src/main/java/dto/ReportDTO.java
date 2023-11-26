e9d5f13a1c1df05ddbe1d95f7973f037e29f9f88
Gyeongrak Choi <peter6617@gmail.com>
2023-11-26 15:53:13 +0900

    make dto

---
+++ src/main/java/dto/ReportDTO.java
@@ -0,0 +1,21 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+
+public class ReportDTO {
+    private int reportId;
+    private String userId;
+    private int brdId;
+    private String reportTitle;
+    private String reportContent;
+    private LocalDate regDate;
+    private LocalDate modDate;
+}
