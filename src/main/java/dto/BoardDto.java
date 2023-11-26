942feedd0c2bae94e43c5aa3706cedfa35b3b0a7
Jae <jaehyuck009603@gmail.com>
2023-11-26 15:38:44 +0900

    DTO추가

---
+++ src/main/java/dto/BoardDto.java
@@ -0,0 +1,34 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@ToString
+public class BoardDto {
+    private int brdId;
+
+    private String userId;
+
+    private String title;
+
+    private String content;
+
+    private String ofile;
+
+    private String sfile;
+
+    private LocalDate regDate;
+
+    private int cateMain;
+
+    private int cateSub;
+
+    private int viewCnt;
+
+}
---
+++ src/main/java/dto/UserDTO.java
@@ -0,0 +1,39 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private String userId;
+
+    private String userPwd;
+
+    private String userName;
+
+    private String userEmail;
+
+    private String userCp;
+
+    private String userAddr;
+
+    private String userDaddr;
+
+    private String userSchool;
+
+    private LocalDate lastLogin;
+
+    private LocalDate joinDate;
+
+    private String userGrade;
+
+    private String userStatus;
+
+    private int reportCnt;
+}
