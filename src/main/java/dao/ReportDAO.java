b3e2e78fb3d34cf55f47b672f352a001584e543f
cyj <cyj083386@gmail.com>
2023-12-08 19:37:31 +0900

    report 테이블 DAO 설정, BaordMapper 오류수정

--- src/main/java/dao/ReportDAO.java
+++ src/main/java/dao/ReportDAO.java
@@ -33,6 +33,43 @@ public class ReportDAO {
     return result;
   }
 
+  public int reportSelectCount(Map<String, Object> map) {
+    SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+    ReportMapper mapper = sqlSession.getMapper(ReportMapper.class);
+    int result = mapper.reportSelectCount(map);
+    System.out.println("selectCount - 행 개수 = " + result);
+    sqlSession.close();
+    return result;
+  }
+
+  public List<ReportDTO> reportSelectListPage(Map<String, Object> map) {
+    SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+    ReportMapper mapper = sqlSession.getMapper(ReportMapper.class);
+    List<ReportDTO> result = mapper.reportSelectListPage(map);
+    sqlSession.close();
+    return result;
+  }
+
+  public ReportDTO reportSelectView(String reportId) {
+    SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+    ReportMapper mapper = sqlSession.getMapper(ReportMapper.class);
+    ReportDTO dto = mapper.reportSelectView(reportId);
+    sqlSession.close();
+    return dto;
+  }
+
+  public int reportDelete(String reportId) {
+    SqlSession sqlSession = MyBatisSessionFactory.getSqlSession();
+    ReportMapper mapper = sqlSession.getMapper(ReportMapper.class);
+    int result = mapper.reportDelete(reportId);
+    if (result == 1) {
+      sqlSession.commit();
+    } else {
+      System.out.println("report 삭제 중 오류 발생...");
+    }
+    return result;
+  }
+
 
 
   //최재혁
