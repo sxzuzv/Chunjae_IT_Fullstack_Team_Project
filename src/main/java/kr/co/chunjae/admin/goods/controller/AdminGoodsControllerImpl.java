c9aa0846151b61ade8879ac26585ffb65b18f42d
choi\cyj <cyj083386@gmail.com>
2024-01-14 23:32:13 +0900

    feat(feature/goods): 상품 등록폼 유효성 검증수정,messages.properties 파일추가

--- src/main/java/kr/co/chunjae/admin/goods/controller/AdminGoodsControllerImpl.java
+++ src/main/java/kr/co/chunjae/admin/goods/controller/AdminGoodsControllerImpl.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+
 import java.io.File;
 import java.util.Enumeration;
 import java.util.HashMap;
