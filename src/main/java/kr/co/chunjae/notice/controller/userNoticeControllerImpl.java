1c5eaea9ac2bf745cd4ca70ad82768f8374d6967
soojin <pofoxz@gmail.com>
2024-01-18 16:54:07 +0900

    Fix(feature/admin): 사용자 접근 공지사항 추가

---
+++ src/main/java/kr/co/chunjae/notice/controller/userNoticeControllerImpl.java
@@ -0,0 +1,62 @@
+package kr.co.chunjae.notice.controller;
+
+import kr.co.chunjae.admin.notice.vo.NoticeVO;
+import kr.co.chunjae.notice.service.userNoticeService;
+import kr.co.chunjae.page.pageVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/notice")
+public class userNoticeControllerImpl {
+    private final userNoticeService noticeService;
+
+    // 게시글 번호를 클릭할 시, 상세 내용을 출력한다.
+    // 조회수 계산 기능을 적용하며, '목록보기' 시 직전에 보고 있던 페이지 번호로 돌아간다.
+    @GetMapping("/noticeDetail.do")
+    public String noticeDetail(@RequestParam("brd_id") Long brdId, @RequestParam(value = "page", required = false, defaultValue = "1") int page, HttpServletRequest request, Model model) {
+        String viewName=(String)request.getAttribute("viewName");
+
+        // 조회수(view_cnt) 기능을 적용한다.
+        noticeService.updateViewCnt(brdId);
+
+        // 선택된 게시글 번호에 대한 상세 내용을 추출하여 저장한다.
+        NoticeVO noticeVO = noticeService.noticeDetail(brdId);
+
+        model.addAttribute("noticeDetail", noticeVO);
+        // '목록보기' 시 활용할 직전 페이지 번호를 모델에 저장한다.
+        model.addAttribute("page", page);
+
+        return viewName;
+    }
+
+    // 전체 게시글 리스트를 출력한다. (페이징 적용 O)
+    // 페이징 : 전체 게시글 리스트 보기 시, 페이징을 적용한다.
+    // 초기 리스트 출력 시, 1 페이지를 보여준다.
+    // paging() : /admin/notice/paging?page='페이지 번호'에 대한 요청을 처리한다.
+    @GetMapping("/noticeList.do")
+    public String pagingNoticeList(@RequestParam(value = "page", required = false, defaultValue = "1") int page, HttpServletRequest request, Model model) {
+        String viewName=(String)request.getAttribute("viewName");
+
+        // 파라미터로 받아온 페이지(번호)에 보여줄 데이터를 추출하여 저장한다.
+        List<NoticeVO> noticeList = noticeService.paging(page);
+
+        System.out.println(noticeList);
+
+        // 페이지 하단에 출력할 숫자를 VO에 지정한다.
+        // 파라미터로 받아온 페이지 번호에 해당하는 리스트만 출력하고, 다른 페이지 숫자는 단순 출력만 한다. (링크 설정 X)
+        pageVO pageVO = noticeService.pagingParam(page);
+
+        model.addAttribute("noticeList", noticeList);
+        model.addAttribute("paging", pageVO);
+        
+        return viewName;
+    }
+
+}
---
+++ src/main/java/kr/co/chunjae/notice/dao/userNoticeDAOImpl.java
@@ -0,0 +1,37 @@
+package kr.co.chunjae.notice.dao;
+
+import kr.co.chunjae.admin.notice.vo.NoticeVO;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class userNoticeDAOImpl implements userNoticeDAO {
+
+    private final SqlSession sqlSession;
+
+    @Override
+    public void updateViewCnt(Long brdId) {
+        sqlSession.update("mapper.notice.updateViewCnt", brdId);
+    }
+
+    @Override
+    public NoticeVO noticeDetail(Long brdId) {
+        return sqlSession.selectOne("mapper.notice.noticeDetail", brdId);
+    }
+
+    @Override
+    public List<NoticeVO> paging(Map<String, Integer> pagingParams) {
+        return sqlSession.selectList("mapper.notice.pagingNoticeList", pagingParams);
+    }
+
+    @Override
+    public int noticeCount() {
+        return sqlSession.selectOne("mapper.notice.noticeCount");
+    }
+
+}
